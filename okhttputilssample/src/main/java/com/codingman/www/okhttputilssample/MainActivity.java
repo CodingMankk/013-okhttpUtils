0fc03c33ca2976c127c93cb9d86ba81795c6fad7
wsxingjun <1185889308@qq.com>
2018-07-04 16:44:45 +0800

    [1]异步get请求使用；[2] getfile 文件下载；[3] getImage下载并加载；[4]postJson；[5]postFile；[6]Post表单形式上传文件（服务器端代码不知道该怎么写？？？）

--- okhttputilssample/src/main/java/com/codingman/www/okhttputilssample/MainActivity.java
+++ okhttputilssample/src/main/java/com/codingman/www/okhttputilssample/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.orhanobut.logger.Logger;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.BitmapCallback;
@@ -17,8 +18,11 @@ import com.zhy.http.okhttp.callback.FileCallBack;
 import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
+import okhttp3.MediaType;
 import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTv;
     private ProgressBar mProgressBar;
     private ImageView mIv;
+
+    private String mBaseUrl = "http://192.168.0.12:8080/okhttpDemo/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
         String url1 = "http://www.391k.com/api/xapi.ashx/info" +
                 ".json?key=bd_hyrzjjfb4modhj&size=10&page=1";
         OkHttpUtils.get()
-                .url(url0)
+//                .url(url0)
+                .url(mBaseUrl+"login?username=OzTaking&password=8888") //本地服务器请求
                 .build()
                 .execute(new TCallBack());
     }
@@ -59,18 +67,35 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * [2] getfile 文件下载
-     * 经测试：下载到了 /storage/sdcard1/21068.txt目录下
+     *
+     * 【说明】
+     * 【1】在android6.0 手机找不到文件；
+     * 【2】在android 4.4 经测试：下载到了 /storage/sdcard1/21068.txt目录下
+     * 【3】在模拟器上android 8.0 需要动态申请权限，会报以下错误：
+     *        /storage/emulated/0 (Permission denied) 权限已开，写入sd卡仍报错的解决办法
+     *  [模拟器解决办法]打开虚拟机的Setting–>Apps–>找到你的应用–>点击Permissions–>将需要的权限手动打开，问题解决。
      */
     public void okHttpDownLoadFile(View view) {
         String url = "http://txt.99dushuzu.com/download-txt/3/21068.txt";
-        String url1 = "https://github.com/hongyangAndroid/okhttp-utils/blob/master/okhttputils" +
-                "-2_4_1.jar?raw=true";
+        String mBaseUrl1 = "http://192.168.0.12:8080/1.txt"; //本地tomcat服务器文件；
+        String mBaseUrl2 = "http://192.168.0.12:8080/1.jpg"; //本地tomcat服务器图片；
+        String mBaseUrl3 = "http://down.360safe.com/360Root/360RootSetup.exe"; //网络文件；
+        String mBaseUrl4 = "https://qqrjtu.xy1758.com/mirrors/win10/8060/windows_10_ultimate_x64_2018.iso"; //网络文件；大文件
+
         OkHttpUtils.get()
-                .url(url)
+                .url(mBaseUrl4)
+//                .url(mBaseUrl3)
+//                .url(mBaseUrl2)
+//                .url(mBaseUrl1)
+//                .url(url)
                 .build()
-                .execute(new FileCallBack(Environment.getExternalStorageDirectory()
-                        .getAbsolutePath(),
-                        "21068.txt") {
+//                .execute(new FileCallBack(Environment.getExternalStorageDirectory()
+//                        .getAbsolutePath(),
+//                        "okHttpDownLoadFile-测试文件.txt")
+                        .execute(new FileCallBack(Environment.getExternalStorageDirectory()
+                                .getAbsolutePath(),
+                                "windows_10_ultimate_x64_2018.iso")
+                        {
                     @Override
                     public void onError(Call call, Exception e, int id) {
                         Logger.e("onError :" + e.getMessage());
@@ -89,16 +114,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * [3] getfile 文件下载
+     * [3] getImage下载并加载
      */
     public void okHttpGetImage(View view){
         mTv.setText("");
         String url ="http://b.hiphotos.baidu.com/image/pic/item/0ff41bd5ad6eddc4f8daa30935dbb6fd52663306.jpg";
+        String mBaseUrlPic1 = "http://192.168.0.12:8080/1.jpg"; //本地tomcat服务器图片；
+        String mBaseUrlPic2 = "http://192.168.0.12:8080/2.jpg"; //本地tomcat服务器图片；
+        String mBaseUrlPic3 = "http://192.168.0.12:8080/3.jpg"; //本地tomcat服务器图片；
+
         OkHttpUtils.get()
-                .url(url)
+//                .url(url)
+                .url(mBaseUrlPic3)
+//                .url(mBaseUrlPic2)
+//                .url(mBaseUrlPic1)
                 .tag(this)
                 .build()
-                .connTimeOut(2000)
+                .connTimeOut(2000)  //默认是10s，单位MILLISECONDS
                 .readTimeOut(2000)
                 .writeTimeOut(2000)
                 .execute(new BitmapCallback() {
@@ -127,19 +159,131 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * [4]postJson
+     *  将数据转化为json然后post到服务器；
+     */
+    public void okHttpPostString(View view){
+        String url = mBaseUrl + "postString";
+        OkHttpUtils.postString()
+                .url(url)
+                .mediaType(MediaType.parse("application/json; charset=utf-8"))
+                .content(new Gson().toJson(new User("OzTaking","12345")))
+                .build()
+                .execute(new TCallBack());
+    }
+
+    /**
+     * [5]postFile
+     * fileName0:post一张图片
+     * fileName1:osot一个exe文件；
+     */
+
+    public void okHttpPostImg(View view){
+
+        String fileName0="1.jeg";
+        String fileName1="360RootSetup.exe";
+
+        mProgressBar.setProgress(0);
+
+        String url = mBaseUrl +"postFile";
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName0);
+        File file2 = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName1);
+        Logger.d(Environment.getExternalStorageDirectory());
+        if (!file2.exists()){
+            Toast.makeText(getApplicationContext(),"PostImg文件不存在",Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        OkHttpUtils.postFile()
+                .url(url)
+                .file(file2)
+                .build()
+                .execute(new TCallBack());
+    }
 
 
+    /**
+     * [6]Post表单形式上传文件:
+     * [header+params+文件]
+     * [注意]服务器端代码不知道该怎么写？？？
+     */
+
+    public void postFormHeaderParamsFile(View view){
+        String url = mBaseUrl + "okHttpPostString";
+
+        String FileName = "360RootSetup.exe";
+        File file = new File(Environment.getExternalStorageDirectory(), FileName);
+        if (!file.exists()){
+            Toast.makeText(getApplicationContext(),"postForm文件不存在",Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        //params
+        Map<String, String> params = new HashMap<>();
+        params.put("username","OzTaking");
+        params.put("password","88888888");
+
+        //headers
+        Map<String, String> headers = new HashMap<>();
+        headers.put("APP-Key","APP-Secret888");
+        headers.put("APP-Secret","APP-Secret666");
+
+        OkHttpUtils.post()
+                .addFile("androidFile",FileName,file)
+                .url(url)
+                .params(params)
+                .headers(headers)
+                .build()
+                .execute(new TCallBack());
+    }
 
 
+    /**
+     * [7]https的验证
+     *对于Https
+     *依然是通过配置即可，框架中提供了一个类HttpsUtils
+     *
+     *设置可访问所有的https网站
+     *HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(null, null, null);
+     *OkHttpClient okHttpClient = new OkHttpClient.Builder()
+     *        .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager)
+     *         //其他配置
+     *         .build();
+     *OkHttpUtils.initClient(okHttpClient);
 
+     *设置具体的证书
+     *HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(证书的inputstream, null, null);
+     *OkHttpClient okHttpClient = new OkHttpClient.Builder()
+     *        .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager))
+     *         //其他配置
+     *         .build();
+     *OkHttpUtils.initClient(okHttpClient);
 
+     *双向认证
+     *HttpsUtils.getSslSocketFactory(
+     *	证书的inputstream,
+     *	本地证书的inputstream,
+     *	本地证书的密码)
+     *同样的，框架中只是提供了几个实现类，你可以自行实现SSLSocketFactory，传入sslSocketFactory即可
+     */
 
+    public void getHttpsHtml(View view){
+        String url = "http://www.12306.cn/mormhweb/";
+        String url1 = "https://kyfw.12306.cn/otn/";
+        OkHttpUtils
+                .get()
+                .url(url1)
+                .build()
+                .connTimeOut(2000)
+                .readTimeOut(2000)
+                .writeTimeOut(2000)
+                .execute(new TCallBack());
 
-
-
-
-
-
+    }
 
 
 
@@ -156,6 +300,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBefore(Request request, int id) {
             setTitle("Loading");
+
         }
 
         @Override
@@ -175,10 +320,10 @@ public class MainActivity extends AppCompatActivity {
             mTv.setText(response);
         }
 
-        //文件下载时进度使用；
+        //文件get/post时进度都会显示-某些情况下，因为速度比较快，进度条显示一闪而过；
         @Override
         public void inProgress(float progress, long total, int id) {
-            mProgressBar.setProgress((int) (progress * 100));
+           mProgressBar.setProgress((int) (progress * 100));
         }
     }
 
